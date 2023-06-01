6b28d20fec4936aa16a3060c94de65a1c0edfe82
lei <1650321704@qq.com>
2023-06-01 23:22:41 +0800

    添加了波次提示

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public static int CountEnemyAlive = 0;
+    public Wave[] waves;
+    public Transform START;
+    public int now_wave;
+    public float waveRate = 0.2f;
+    private Coroutine coroutine;
+
+    void Start()
+    {
+        coroutine = StartCoroutine(SpawnEnemy());
+    }
+    public void Stop()
+    {
+        StopCoroutine(coroutine);
+    }
+    IEnumerator SpawnEnemy()
+    {
+        now_wave = 0;
+        foreach (Wave wave in waves)
+        {
+            now_wave++;
+            GameObject.Find("Canvas/Wave").GetComponent<Text>().text="Wave:"+now_wave+"/4";
+            for (int i = 0; i < wave.count; i++)
+            {
+                GameObject.Instantiate(wave.enemyPrefab, START.position, Quaternion.identity);
+                CountEnemyAlive++;
+                if(i!=wave.count-1)
+                    yield return new WaitForSeconds(wave.rate);
+            }
+            while (CountEnemyAlive > 0)
+            {
+                yield return 0;
+            }
+            yield return new WaitForSeconds(waveRate);
+        }
+        while (CountEnemyAlive > 0)
+        {
+            yield return 0;
+        }
+        GameManager.Instance.Win();
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject endUI;
+    public Text endMessage;
+
+    public static GameManager Instance;
+    private EnemySpawner enemySpawner;
+    bool isPaused = false;
+
+
+    void Awake()
+    {
+        Instance = this;
+        enemySpawner = GetComponent<EnemySpawner>();
+    }
+
+    public void Win()
+    {
+        GameObject.Find("Canvas/Wave").GetComponent<Text>().text = "";
+        endUI.SetActive(true);
+        endMessage.text = "胜 利";
+    }
+    public void Failed()
+    {
+        GameObject.Find("Canvas/Wave").GetComponent<Text>().text = "";
+        enemySpawner.Stop();
+        endUI.SetActive(true);
+        endMessage.text = "失 败";
+    }
+
+    public void OnButtonRetry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex );
+    }
+    public void OnButtonMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    //---------------------------------------------------------曾颉 add   PauseGame-----------------------------------------------------------------
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+           
+            //打印isPaused
+            Debug.Log(isPaused);
+            
+            if (isPaused)
+            {
+                // Pause the game
+                endUI.SetActive(true);
+                endMessage.text = "暂 停";
+                Time.timeScale = 0;
+                endUI.GetComponent<Animator>().updateMode = AnimatorUpdateMode.UnscaledTime;
+            }
+            else
+            {
+                // Unpause the game
+                Time.timeScale = 1;
+                endUI.SetActive(false);
+                endUI.GetComponent<Animator>().updateMode = AnimatorUpdateMode.Normal;
+            }
+        }
+    }
+
+
+    //---------------------------------------------------------曾颉 add   PauseGame-----------------------------------------------------------------
+
+
+
+
+}
